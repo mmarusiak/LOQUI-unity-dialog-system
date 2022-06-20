125c80215f86c285f710a4401adc43f5a312c861
Marcel <marcel.marusiak@gmail.com>
2022-06-20 15:24:32 +0200

    Messing with unity editor window

--- Package/Scripts/Visual Editor/DialogWindow.cs
+++ Package/Scripts/Visual Editor/DialogWindow.cs
@@ -1,18 +1,22 @@
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
 public class DialogWindow : EditorWindow
 {
-    Rect windowRect = new Rect (100 + 100, 100, 100, 100);
-    Rect windowRect2 = new Rect (100, 100, 100, 100);
+    private bool _creatingNode = false;
+    string _newNodeTitle = "", _newNodeText = "";
+    private int _selectedNodeType = -1, _lastTouchedWindow;
 
-    private bool _addingNode = false;
-    private Vector2 _scrollPosition = Vector2.zero;
-    private int _idOfLastTouhedNode;
+    public List<DialogNode> DialogNodes = new List<DialogNode>();
 
-    public List<DialogNode> NodeList = new List<DialogNode>();
-
+    private Color32[] _colorPallete = new[]
+    {
+        new Color32(38, 70, 83, 255),
+        new Color32(231, 111, 81, 255),
+        new Color32(233, 196, 106, 255)
+    };
 
     [MenuItem("Window/Dialog system window")]
     public static void Init()
@@ -20,76 +24,69 @@ public class DialogWindow : EditorWindow
         DialogWindow window = GetWindow<DialogWindow>("Dialog editor");
         window.Show();
         
-        GUIContent content = new GUIContent("Dialog editor");
-        window.titleContent = content;
+        
     }
 
     private void OnGUI()
     {
-        if (_addingNode)
+        // Create new node
+        if (GUI.Button(new Rect(0, 0, position.width / 2, 60), "ADD NODE"))
         {
-            string _title = "", _text = "";
-            GUILayout.BeginHorizontal();
-            _title = EditorGUILayout.TextField("Node title", _title);
-            _text = EditorGUILayout.TextField("Node text", _text);
-            GUILayout.EndHorizontal();
-        }
-        
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Add node"))
-        {
-            _addingNode = !_addingNode;
-            NodeList.Add(new DialogNode("test", "test123,", DialogNode.NodeType.Passive));
+            _creatingNode = !_creatingNode;
+
+            if (_selectedNodeType >= 0 && _newNodeTitle != "" && _newNodeText != "")
+            {
+                DialogNodes.Add(new DialogNode(_newNodeTitle, _newNodeText, (DialogNode.NodeType)_selectedNodeType));
+            }
+
+            _selectedNodeType = -1;
+            _newNodeTitle = "";
+            _newNodeText = "";
         }
 
-        if (GUILayout.Button("Remove node"))
+        // Remove existing node
+        if (GUI.Button(new Rect(position.width / 2, 0, position.width / 2, 60), "REMOVE NODE"))
         {
-            NodeList.RemoveAt(_idOfLastTouhedNode);
+            DialogNodes.RemoveAt(_lastTouchedWindow);
         }
-        GUILayout.EndHorizontal();
 
-        _scrollPosition = GUI.BeginScrollView(
-            new Rect(0, 0, position.width-1, position.height), 
-                _scrollPosition, new Rect(0, 0, position.width, position.height));
+        if (_creatingNode)
+        {
+            // Title "box"
+            GUI.Label(new Rect(0, 60, position.width, 40), 
+                "Enter dialog node title", EditorStyles.boldLabel);
+            _newNodeTitle = GUI.TextField(new Rect(position.width / 2, 60,
+                        position.width / 2, 40), _newNodeTitle, 10);
+            
+            // Text "box"
+            GUI.Label(new Rect(0, 100, position.width, 40), 
+                "Enter dialog node text", EditorStyles.boldLabel);
+            _newNodeText = GUI.TextField(new Rect(position.width / 2, 100,
+                position.width / 2, 40), _newNodeText, 40);
+            
+            // Dialog type popup
+            _selectedNodeType = EditorGUI.Popup(new Rect(3*position.width/8, 140, position.width/4, 40),
+                _selectedNodeType, new string[]
+                {
+                    DialogNode.NodeType.Passive.ToString(),
+                    DialogNode.NodeType.MultipleChoice.ToString()
+                });
+        }
         
-        Handles.BeginGUI();
-        Handles.DrawBezier(windowRect.center, 
-            windowRect2.center, 
-            new Vector2(windowRect.xMax + 50f,windowRect.center.y), 
-            new Vector2(windowRect2.xMin - 50f,windowRect2.center.y), 
-            Color.red,null,5f);
-        
-        Handles.EndGUI();
-
         BeginWindows();
-        for (int i = 0; i < NodeList.Count; i++)
+        for (int i = 0; i < DialogNodes.Count; i++)
         {
-            NodeList[i].NodeRect = 
-                KeepInWindow(GUI.Window(i, NodeList[i].NodeRect,
-                    WindowFunction, NodeList[i].Title));
+            GUI.color = _colorPallete[(int) DialogNodes[i].DialogNodeType];
+            DialogNodes[i].NodeRect = GUI.Window(i, DialogNodes[i].NodeRect,
+                    WindowFunction, DialogNodes[i].Title);
         }
         EndWindows();
-        GUI.EndScrollView();
+
+        
     }
     
     void WindowFunction (int windowID)
     {
         GUI.DragWindow();
     }
-
-    Rect KeepInWindow(Rect rect)
-    {
-        if (rect.position.x > position.width)
-            rect.position = new Vector2(position.width, rect.position.y);
-        if (rect.position.y > position.height)
-            rect.position = new Vector2(rect.position.x, position.height);
-
-        if (rect.position.x < 0)
-            rect.position = new Vector2(0, rect.position.y);
-        if (rect.position.y < 0)
-            rect.position = new Vector2(rect.position.x, 0);
-            
-        return rect;
-    }
-    
 }
